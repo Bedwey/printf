3db0370896f2239eb734720352de596507023a2a
Bedwey <bedweyy@gmail.com>
2023-04-23 10:40:18 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ 0-printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _printf - write output to stdout,
+ * the standard output stream
+ *
+ * @format: is a character string.
+ *
+ * Return: the number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int len;
+	va_list ap;
+	args_handle_t argsList[] = {
+			{"c", print_char},
+			{"s", print_string},
+			{"%", print_percent}
+		};
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+
+	len = manager(format, ap, argsList);
+
+	va_end(ap);
+
+	return (len);
+}
